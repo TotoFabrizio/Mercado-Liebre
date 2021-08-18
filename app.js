3ea11e4528ef47fb6d3865f58253aa864b6c041c
TotoFabrizio <tomas.fabrizio02@gmail.com>
2021-08-18 19:18:12 -0300

    Proyecto mercado liebre

---
+++ app.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const path = require("path");
+const app = express();
+
+const rutaPublic = path.resolve(__dirname, "./public");
+app.use(express.static(rutaPublic));
+
+app.listen(3000, () => {
+  console.log("Servidor corriendo en puerto 3000");
+});
+
+app.get("/", (req, res) => {
+  let ruta = path.resolve(__dirname, "./views/home.html");
+  res.sendFile(ruta);
+});
+
+app.get("/register", (req, res) => {
+  let ruta = path.resolve(__dirname, "./views/register.html");
+  res.sendFile(ruta);
+});
+
+app.get("/login", (req, res) => {
+  let ruta = path.resolve(__dirname, "./views/login.html");
+  res.sendFile(ruta);
+});
